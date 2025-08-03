6963a0048d621dc477542837b97dc964d3f5f21f
suzanwanjiku <susanwanjiku093@gmail.com>
2025-08-03 08:00:48 -0700

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,8 @@
+--- Question 1:
+--- Write an SQL query to create a new database called salesDB
+CREATE DATABASE salesDB;
+
+--- Question 2:
+--- Drop (delete) a database called demo
+CREATE DATABASE demo;
+DROP DATABASE demo;
